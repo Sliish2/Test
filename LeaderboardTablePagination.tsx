1bc94f5a08560e503b8f1007e2a2982d07df7033
Sliish2 <104549419+Sliish2@users.noreply.github.com>
2025-08-14 15:10:07 +0300

    new_file

---
+++ LeaderboardTablePagination.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import styles from "../leaderboards.module.scss";
+
+type Props = {
+  entriesPerPage: number;
+  length: number;
+  handlePagination: (arg0: number) => void;
+  currentPage: number;
+};
+
+const LeaderbordTablePagination: React.FC<Props> = ({
+  entriesPerPage = 20,
+  length,
+  handlePagination,
+  currentPage,
+}) => {
+  const paginationNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(length / entriesPerPage); i++) {
+    // @ts-ignore
+    paginationNumbers.push(i);
+  }
+
+  if (paginationNumbers.length === 1) {
+    return null;
+  }
+
+  return (
+    <div className={styles.pagination}>
+      {paginationNumbers.map((pageNumber) => (
+        <button
+          key={pageNumber}
+          onClick={() => handlePagination(pageNumber)}
+          className={currentPage === pageNumber ? `${styles.active}` : ""}
+        >
+          {pageNumber}
+        </button>
+      ))}
+    </div>
+  );
+};
+export default LeaderbordTablePagination;
